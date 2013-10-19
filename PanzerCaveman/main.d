214de5e4414b5473afd7ebb3bb40bbe45c263e74
GenoB <s.schekochikhin@gmail.com>
2013-10-18 21:25:10 -0700

    Updated readme. Got all libs to load

--- PanzerCaveman/main.d
+++ PanzerCaveman/main.d
@@ -1,21 +1,35 @@
 module main;
 
 import std.stdio;
+import derelict.util.exception;
 import derelict.opengl3.gl3;
 import derelict.sdl2.sdl;
 import derelict.assimp3.assimp;
 import derelict.lua.lua;
-import derelict.devil.il;
+import derelict.freeimage.freeimage;
 import awesomium.dapi;
 
 int main(string[] argv)
 {
-	DerelictSDL2.load();
-	DerelictGL3.load();
-	DerelictASSIMP3.load();
-	DerelictLua.load();
-	DerelictIL.load();
+	try{
+		DerelictSDL2.load();
+		DerelictGL3.load();
+		DerelictASSIMP3.load();
+		DerelictLua.load();
+		DerelictFI.load();
+	}catch (DerelictException de)
+	{
+		writeln("failed to load lib %s", de.msg);
+		return -2;
+	}
 
-   writeln("Hello D-World!");
-   return 0;
+	if(SDL_Init(SDL_INIT_EVERYTHING) == -1)
+	{
+		writeln("SDL failed to init");
+		return -1;
+	}
+
+	return 0;
 }
+
+
