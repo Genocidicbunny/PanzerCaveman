ebae8caa3cf9a2553dfe6b3eca814a0909fc3148
GenoB <s.schekochikhin@gmail.com>
2013-12-11 01:28:42 -0800

    Updated main and proj file

--- PanzerCaveman/main.d
+++ PanzerCaveman/main.d
@@ -7,13 +7,12 @@ int main(string[] argv)
 	try{
 		auto pzc_game = new Game();
 		pzc_game.initialize();
+		pzc_game.run();
 		pzc_game.shutdown();
 	} catch(Exception e) {
 		writeln(e);
 	}
-	while(true) {
 	
-	}
 	return 0;
 }
 
